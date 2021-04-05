b95e6592222dd0bc78714d64265e0b7fe6111144
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-04-05 17:53:45 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -4,17 +4,16 @@ console.log(`В списке ${allItems.length} категории.`);
 
 
 const listItem = document.querySelector('.item')
-console.log(allItems[0].lastElementChild.children.length)
-console.log('Категория:', listItem.firstElementChild.textContent);
-console.log(`Количество элементов: ${listItem.lastElementChild.children.length}`);
+// console.log(allItems[0].lastElementChild.children.length)
+console.log('Категория:', allItems[0].firstElementChild.textContent);
+console.log(`Количество элементов: ${allItems[0].lastElementChild.children.length}`);
 
 
-console.log('Категория:', listItem.nextElementSibling.firstElementChild.textContent);
-console.log(`Количество элементов: ${listItem.nextElementSibling.lastElementChild.children.length}`);
+console.log('Категория:', allItems[1].firstElementChild.textContent);
+console.log(`Количество элементов: ${allItems[1].lastElementChild.children.length}`);
 
-console.log('Категория:', listItem.nextElementSibling.nextElementSibling.firstElementChild.textContent);
-console.log(`Количество элементов: ${listItem.nextElementSibling.nextElementSibling
-    .lastElementChild.children.length}`);
+console.log('Категория:',  allItems[2].firstElementChild.textContent);
+console.log(`Количество элементов: ${allItems[2].lastElementChild.children.length}`);
 
 
 
