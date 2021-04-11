33168db3fd0cd7ddd0e9321f91e481a81f16c03e
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-04-11 13:12:51 +0100

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,29 +1,50 @@
-const checkInput = document.querySelector('#validation-input');
-console.log(checkInput);
+// const checkInput = document.querySelector('#validation-input');
+// console.log(checkInput);
 
-checkInput.addEventListener('blur', onInput);
-function onInput(event) {
-  checkInput.classList.add("valid", 'invalid')
+// checkInput.addEventListener('blur', onInput);
+// function onInput(event) {
+//   checkInput.classList.add("valid", 'invalid')
 
-  console.log(event.currentTarget.value.length)
+//   console.log(event.currentTarget.value.length)
 
-  if (event.currentTarget.value.length == checkInput.dataset.length) {
-    checkInput.classList.toggle('invalid')
-  }
-}
-console.log(checkInput.dataset.length)
+//   if (event.currentTarget.value.length == checkInput.dataset.length) {
+//     checkInput.classList.toggle('invalid')
+//   }
+// }
+// console.log(checkInput.dataset.length)
+
 
 
 // Второй Вариант
 
+
+// const checkInput = document.querySelector('#validation-input');
+// console.log(checkInput);
+
+// checkInput.addEventListener('blur', onInput);
+// function onInput(event) {
+//   // checkInput.classList.add("valid", 'invalid')
+
+//   console.log(event.currentTarget.value.length)
+
+//   if (event.currentTarget.value.length<checkInput.dataset.length || event.currentTarget.value.length>checkInput.dataset.length ) {
+//     checkInput.classList.add('invalid')
+//   } else {
+//     checkInput.classList.remove('invalid');
+//     checkInput.classList.add('valid');
+//   }
+  
+// }
+// console.log(checkInput.dataset.length)
+
 //     const inputEl = document.querySelector("input#validation-input");
-// //const inputEl = document.querySelector("input");
+
 // inputEl.addEventListener("blur", onInputChange);
 // console.log(inputEl);
 // inputEl.style.border = ' 3px solid #BDBDBD'
 // function onInputChange(event) {
 //   console.log(event.currentTarget.value);
-//   if (event.currentTarget.value.length === 6) {
+//   if (event.currentTarget.value.length == inputEl.dataset.length) {
 //       inputEl.style.borderColor = ' #4CAF50';
 //   } else inputEl.style.borderColor = '#F44336';
   
